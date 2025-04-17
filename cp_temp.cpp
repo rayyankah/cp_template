2dc74428998b5a446f49fa58b084c30303d9256b
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-04-17 12:30:37 +0600

    Add files via upload

---
+++ cp_temp.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vvi vector<vector<int>>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define pi pair<int, int>
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define tk_input(a) rep(i, 0, n) cin >> a[i]
+#define repb(i, a, b) for (int i = a; i >= b; i--)
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+const int MOD = 1e9 + 7;
+#define nl '\n'
+#define precise(x) fixed << setprecision(x)
+
+signed main()
+{
+    fast;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vi a(n);
+
+        rep(i, 0, n) cin >> a[i];
+    }
+}
