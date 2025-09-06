5e7ae246c4e64f9fb63b7f33eedf7a6e31048270
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-07 00:36:33 +0600

    Update cp_temp.cpp

--- cp_temp.cpp
+++ cp_temp.cpp
@@ -1,7 +1,15 @@
+// ================ Author: Rayyan Khalil ================
+
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 using namespace std;
 
 #define vvi vector<vector<int>>
+#define vll vector<long long>
+#define si set<int>
+#define mii map<int, int>
 #define vi vector<int>
 #define vpi vector<pair<int, int>>
 #define pi pair<int, int>
@@ -9,35 +17,250 @@ using namespace std;
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = a; i < b; i++)
-#define tk_input(a) rep(i,0,n)cin>>a[i]
+#define tk_input(a) rep(i, 0, n) cin >> a[i]
 #define repb(i, a, b) for (int i = a; i >= b; i--)
-#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
 #ifdef DEBUG
-#define debug(...) cerr<<"["<< #__VA_ARGS__ <<"]: ", debug_print(__VA_ARGS__)
-void debug_print(){cerr<<endl; }
-template<typename T, typename... Args>
-void debug_print(T t, Args... args) {
-    cerr<< t;
-    if(sizeof...(args)) cerr<<", ";
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_print(__VA_ARGS__)
+void debug_print() { cerr << endl; }
+template <typename T, typename... Args>
+void debug_print(T t, Args... args)
+{
+    cerr << t;
+    if (sizeof...(args))
+        cerr << ", ";
     debug_print(args...);
 }
 #else
 #define debug(...)
 #endif
-const int MOD = 1e9 + 7;
+
+#define myCeil(a, b) (a + b - 1) / b
+const int MOD = 1000000007;
 #define nl '\n'
 #define precise(x) fixed << setprecision(x)
-
-void solve(){
-   
+#define int ll
+int binMul(int a, int b, int m)
+{
+    int res = 0;
+    a %= m;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = (res + a) % m;
+        }
+        a = (a + a) % m;
+        b >>= 1;
+    }
+    return res;
 }
-signed main() {
-    fast;
-    
-    int t=1;
-    cin >> t;
-    while (t--) {
-     solve();      
 
+int binExp(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = (1LL * res * a) % MOD;
+        }
+        a = (1LL * a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+int extended_euclidean_gcd(int a, int b, int &x, int &y)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclidean_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+int inverse_mod(int a, int m){
+    int x,y;
+    int g = extended_euclidean_gcd(a,m,x,y);
+    if(g!=1)return -1;
+    else{
+        x = (x%m+m)%m;
+        return x;
+    }
+}
+
+template <class T>
+using vv = vector<vector<T>>;
+template <class T>
+using v = vector<T>;
+
+// ------------------ SAFE unordered_map helpers ------------------
+struct custom_hash
+{
+    static inline uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15ULL;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const noexcept
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return (size_t)splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <class K, class V>
+using umap = unordered_map<K, V, custom_hash>;
+template <class T>
+using uset = unordered_set<T, custom_hash>;
+
+template <class Map>
+inline void secure_reserve(Map &m, size_t expected_size, float lf = 0.25f)
+{
+    m.max_load_factor(lf);
+    m.reserve(expected_size);
+}
+
+template <class T>
+void read(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+template <class T>
+void read(vv<T> &a)
+{
+    for (auto &r : a)
+        for (auto &x : r)
+            cin >> x;
+}
+
+template <class A, class B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &a)
+{
+    os << "[";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "]";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class K, class V>
+ostream &operator<<(ostream &os, const map<K, V> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &kv : a)
+    {
+        os << s << kv.first << ": " << kv.second;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+
+template <class K, class V, class H, class Eq, class Alloc>
+ostream &operator<<(ostream &os, const unordered_map<K, V, H, Eq, Alloc> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &kv : a)
+    {
+        os << s << kv.first << ": " << kv.second;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class T, size_t N>
+ostream &operator<<(ostream &os, const array<T, N> &a)
+{
+    os << "[";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "]";
+    return os;
+}
+
+// template<typename T>
+// using ordered_set = tree<T, null_type, //less<T>, rb_tree_tag, //tree_order_statistics_node_update>;
+// ans+=b.size()-b.order_of_key(in[i].second+1);
+
+template <class T>
+void debug_print_array(vector<T> &arr)
+{
+    for (auto &elem : arr)
+    {
+        cerr << elem << " ";
+    }
+    cerr << endl;
+}
+
+//================ Code starts here ================
+void solve()
+{
+
+}
+
+signed main()
+{
+    fast;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
     }
 }
