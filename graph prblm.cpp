bb50e69c5f7f9a6801db53e130dc48c37f1dfcae
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-15 10:35:34 +0600

    Update graph prblm.cpp

--- graph prblm.cpp
+++ graph prblm.cpp
@@ -361,3 +361,420 @@ signed main()
 
 
 //cses =->mailman
+// ================ Author: Rayyan Khalil ================
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define si set<int>
+#define mii map<int, int>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define pi pair<int, int>
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define tk_input(a) rep(i, 0, n) cin >> a[i]
+#define repb(i, a, b) for (int i = a; i >= b; i--)
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+#ifdef DEBUG
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_print(__VA_ARGS__)
+void debug_print() { cerr << endl; }
+template <typename T, typename... Args>
+void debug_print(T t, Args... args)
+{
+    cerr << t;
+    if (sizeof...(args))
+        cerr << ", ";
+    debug_print(args...);
+}
+#else
+#define debug(...)
+#endif
+
+#define myCeil(a, b) (a + b - 1) / b
+const int MOD = 1000000007;
+#define nl '\n'
+#define precise(x) fixed << setprecision(x)
+#define int ll
+int binMul(int a, int b, int m)
+{
+    int res = 0;
+    a %= m;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = (res + a) % m;
+        }
+        a = (a + a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int binExp(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = (1LL * res * a) % MOD;
+        }
+        a = (1LL * a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+int extended_euclidean_gcd(int a, int b, int &x, int &y)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclidean_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+template <class T>
+using vv = vector<vector<T>>;
+template <class T>
+using v = vector<T>;
+
+// ------------------ SAFE unordered_map helpers ------------------
+struct custom_hash
+{
+    static inline uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15ULL;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const noexcept
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return (size_t)splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <class K, class V>
+using umap = unordered_map<K, V, custom_hash>;
+template <class T>
+using uset = unordered_set<T, custom_hash>;
+
+template <class Map>
+inline void secure_reserve(Map &m, size_t expected_size, float lf = 0.25f)
+{
+    m.max_load_factor(lf);
+    m.reserve(expected_size);
+}
+
+template <class T>
+void read(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+template <class T>
+void read(vv<T> &a)
+{
+    for (auto &r : a)
+        for (auto &x : r)
+            cin >> x;
+}
+
+template <class A, class B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &a)
+{
+    os << "[";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "]";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class K, class V>
+ostream &operator<<(ostream &os, const map<K, V> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &kv : a)
+    {
+        os << s << kv.first << ": " << kv.second;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+
+template <class K, class V, class H, class Eq, class Alloc>
+ostream &operator<<(ostream &os, const unordered_map<K, V, H, Eq, Alloc> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &kv : a)
+    {
+        os << s << kv.first << ": " << kv.second;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class T, size_t N>
+ostream &operator<<(ostream &os, const array<T, N> &a)
+{
+    os << "[";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "]";
+    return os;
+}
+
+// template<typename T>
+// using ordered_set = tree<T, null_type, //less<T>, rb_tree_tag, //tree_order_statistics_node_update>;
+// ans+=b.size()-b.order_of_key(in[i].second+1);
+
+template <class T>
+void debug_print_array(vector<T> &arr)
+{
+    for (auto &elem : arr)
+    {
+        cerr << elem << " ";
+    }
+    cerr << endl;
+}
+const int N = 1e6 + 10;
+vi factorial(N);
+vi inv(N);
+void compute_factorials()
+{
+    factorial[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        factorial[i] = (factorial[i - 1] * i) % MOD;
+    }
+    inv[N - 1] = binExp(factorial[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 0; i--)
+    {
+        inv[i] = (inv[i + 1] * (i + 1)) % MOD;
+    }
+}
+int ncr(int n, int r)
+{
+    if (r > n || r < 0)
+        return 0;
+
+    return (binMul(factorial[n], binMul(inv[r], inv[n - r], MOD), MOD));
+}
+// finding eulerian path/circuit
+const int MAXN = 100005;
+int deg[MAXN];
+bool hasEulerPath(int n)
+{
+    int odd = 0;
+    for (int i = 0; i <= n; ++i)
+        if (deg[i] & 1)
+            ++odd;
+    return (odd == 0 || odd == 2);
+}
+
+// Hierholzer: recursive removal using multiset adjacency
+void findEulerPathOrCircuit(int u, vector<int> &res, vector<multiset<int>> &adj)
+{
+    while (!adj[u].empty())
+    {
+        int v = *adj[u].begin();
+        // remove edge u-v from both sides
+        adj[u].erase(adj[u].begin());
+        auto it = adj[v].find(u);
+        if (it != adj[v].end())
+            adj[v].erase(it);
+        findEulerPathOrCircuit(v, res, adj);
+    }
+    res.push_back(u);
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; ++i)
+        deg[i] = 0;
+
+    vector<pair<int, int>> edges;
+    edges.reserve(m);
+    vector<multiset<int>> adjm(n + 1);
+
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        edges.emplace_back(u, v);
+        adjm[u].insert(v);
+        adjm[v].insert(u);
+        ++deg[u];
+        ++deg[v];
+    }
+    if (n == 2 && m == 1 && edges[0].first == 1 && edges[0].second == 2)
+    {
+        cout << "IMPOSSIBLE\n ";
+        return;
+    }
+
+    if (m == 0)
+    {
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+
+    vector<int> odds;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (deg[i] & 1)
+            odds.push_back(i);
+    }
+    if (odds.size() > 2 || odds.size() == 1)
+    {
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+
+    int start = 1;
+    if (!odds.empty())
+    {
+        if (odds[0] != 1 && odds[1] != 1)
+        {
+            cout << "IMPOSSIBLE\n";
+            return;
+        }
+        // start remains 1
+    }
+
+    vector<int> ans;
+    findEulerPathOrCircuit(start, ans, adjm);
+    reverse(ans.begin(), ans.end());
+
+    if ((int)ans.size() != m + 1)
+    {
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+
+    for (int i = 0; i <= n; ++i)
+    {
+        if (!adjm[i].empty())
+        {
+            cout << "IMPOSSIBLE\n";
+            return;
+        }
+    }
+
+    if (ans[0] != 1)
+    {
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+
+    for (int i = 0; i < (int)ans.size(); ++i)
+    {
+        if (i)
+            cout << ' ';
+        cout << ans[i];
+    }
+    cout << '\n';
+    return;
+}
+
+signed main()
+{
+    fast;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//cses -> level games
+
