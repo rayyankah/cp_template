370e604d8420696d748f5aa9dcfc899c02d81eec
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-16 23:24:07 +0600

    Create dpcode.cpp

---
+++ dpcode.cpp
@@ -0,0 +1,137 @@
+//coins exhibition cf
+
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <bits/stdc++.h>
+#define int   long long
+#define ll long long
+#define down cout<<'\n';
+#define debug cout<<" cucuucucuuu",down
+#define modwwe  int t;cin>>t; while(t--)
+#define bit(i,j) (i>>j&1)
+#define sobit(a) __builtin_popcountll(a)
+#define task2 "top1apio"
+#define task "test"
+#define fin(x) freopen(x".inp","r",stdin)
+#define fou(x) freopen(x".out","w",stdout)
+#define pb push_back
+#define mask(k) (1ll<<k)
+#define checktime   cerr << (double)clock() / CLOCKS_PER_SEC * 1000  << " ms";
+using namespace std;
+#define getchar_unlocked getchar
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+using i128 = __int128;
+int rand(int l, int r)
+{
+    return uniform_int_distribution<int>(l, r)(rd);
+}
+void phongbeo();
+const ll inf = 1e17;
+const int mod2 = 1e9+7;
+//const ll base=67;
+int  n, m, s1, s2, s4, s3, sf, k, s5, s6, s7, s8, s9, mx2, res, dem2 = 0, dem = 0, s33, dem3, dem4, mid, l2,
+                                                                r2, center;
+int  i, s10, s12, k1, k2, k3, s11, lim, w, l, r, dem5, dem6, dem7, dem9, now, root, q, en;
+int el = 19;
+main()
+{
+    ///top1apio
+    if (fopen(task2".inp", "r"))
+    {
+        fin(task2);
+        fou(task2);
+    }
+
+    ///top1apio
+    if (fopen(task".inp", "r"))
+    {
+        fin(task);
+        fou(task);
+    }
+
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    //   cin>>s1;
+    //ll t;cin >> t;while (t--)
+    phongbeo(),down
+}
+struct ib
+{
+    int a,b;
+};
+vector<ib> v[800003];
+vector<int> vc;
+int dp[2][800003];
+int gg[2];
+int last[2];
+int a[800003],b[800003],c[800003],d[800003];
+int add(int x,int y)
+{
+    if(x+y>=mod2)x-=mod2;
+    if(x+y<0)x+=mod2;
+    return x+y;
+}
+int ipow(int x, int p)
+{
+    int  ret = 1, piv = x;
+    while(p)
+    {
+        if(p&1) ret *= piv;
+        piv *= piv;
+        ret %= mod2;
+        piv %= mod2;
+        p >>= 1;
+    }
+    return ret;
+}
+void phongbeo()
+{
+    cin>>k>>n>>m;
+    vc.pb(0);
+    for(int i=1; i<=n; i++)
+        cin>>a[i]>>b[i],vc.pb(a[i]),vc.pb(b[i]);
+    for(int i=1; i<=m; i++)
+        cin>>c[i]>>d[i],vc.pb(c[i]),vc.pb(d[i]);
+    vc.pb(k);
+    sort(vc.begin(),vc.end());
+    vc.erase(unique(vc.begin(),vc.end()),vc.end());
+    for(int i=1; i<=n; i++)
+    {
+        a[i]=lower_bound(vc.begin(),vc.end(),a[i])-vc.begin()+1;
+        b[i]=lower_bound(vc.begin(),vc.end(),b[i])-vc.begin()+1;
+        v[b[i]].pb({0,a[i]});
+    }
+    for(int i=1; i<=m; i++)
+    {
+        c[i]=lower_bound(vc.begin(),vc.end(),c[i])-vc.begin()+1;
+        d[i]=lower_bound(vc.begin(),vc.end(),d[i])-vc.begin()+1;
+        v[d[i]].pb({1,c[i]});
+    }
+    dp[0][1]=ipow(2,vc[1]-vc[0]-1);
+    dp[1][1]=ipow(2,vc[1]-vc[0]-1);
+    gg[0]=dp[0][1];
+    gg[1]=dp[1][1];
+    for(int i=2; i<=vc.size(); i++)
+    {
+        for(auto x:v[i]){
+            if(x.b-1>last[x.a])
+            {
+                for(int j=last[x.a]+1; j<x.b; j++)
+                gg[x.a]=add(gg[x.a],-dp[x.a][j]);
+                last[x.a]=x.b-1;
+            }
+        }
+        if(i!=vc.size())
+        {
+            for(int j=0; j<2; j++)
+            {
+                dp[1-j][i]=add(dp[1-j][i],1ll*ipow(2,vc[i]-vc[i-1]-1)*gg[j]%mod2);
+                dp[j][i]=add(dp[j][i],1ll*gg[j]*(ipow(2,vc[i]-vc[i-1]-1)-1)%mod2);
+            }
+            for(int j=0; j<2; j++)
+                gg[j]=add(gg[j],dp[j][i]);
+        }
+    }
+    cout<<add(gg[0],gg[1]);
+}
