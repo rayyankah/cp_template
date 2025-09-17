b131479870da3fe6e997a41899f0a6743ac4c784
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-17 09:29:52 +0600

    Update dpcode.cpp

--- dpcode.cpp
+++ dpcode.cpp
@@ -197,6 +197,160 @@ void solve()
 
 
 
+
+
+//remove k so that remaining trees maximal
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <memory.h>
+#include <sstream>
+#include <complex>
+#include <cassert>
+#include <climits>
+
+#define REP(i,n) for(int i = 0, _n = (n); i < _n; i++)
+#define REPD(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define FOR(i,a,b) for (int i = (a), _b = (b); i <= _b; i++)
+#define FORD(i,a,b) for (int i = (a), _b = (b); i >= _b; i--)
+#define FORN(i,a,b) for(int i=a;i<b;i++)
+#define FOREACH(it,c) for (__typeof((c).begin()) it=(c).begin();it!=(c).end();it++)
+#define RESET(c,x) memset (c, x, sizeof (c))
+
+#define PI acos(-1)
+#define sqr(x) ((x) * (x))
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define Aint(c) (c).begin(), (c).end()
+#define SIZE(c) (c).size()
+
+#define DEBUG(x) { cerr << #x << " = " << x << endl; }
+#define PR(a,n) {cerr<<#a<<" = "; FOR(_,1,n) cerr << a[_] << ' '; cerr <<endl;}
+#define PR0(a,n) {cerr<<#a<<" = ";REP(_,n) cerr << a[_] << ' '; cerr << endl;}
+#define LL long long
+
+using namespace std;
+
+#define maxn 100111
+
+vector <int> adj[maxn];
+int n, k, weight[maxn], cnt[maxn], dfs_order[maxn];
+long long f[maxn], sum[maxn];
+int __cnt;
+
+bool getChar(char& c) {
+    return (scanf("%c", &c) == 1);
+}
+
+void nextInt(long long& u, char endline, int l, int r) {
+    int sign = 1;
+    long long sum = 0;
+    char c;
+    assert(getChar(c));
+    if (c == '-') sign = -1;
+    else {
+        assert('0' <= c && c <= '9');
+        sum = (c - '0');
+    }
+
+    int cnt = 0;
+
+    for (int i = 1; i <= 15; i++) {
+        assert(getChar(c));
+        if (c == endline) break;
+        assert('0' <= c && c <= '9');
+        sum = sum * 10 + (c - '0');
+        cnt ++;
+        assert(cnt <= 15);
+    }
+
+    sum = sign * sum;
+    assert (l <= sum && sum <= r);
+    u = sum;
+}
+
+void nextInt(int& u, char endline, int l, int r) {
+    long long tmp;
+    nextInt(tmp, endline, l, r);
+    u = tmp;
+}
+
+void dfs(int u) {
+    dfs_order[++__cnt] = u;
+    cnt[u] = 1;
+
+    for (int i = 0; i < (int) adj[u].size(); i++) {
+        int v = adj[u][i];
+        if (cnt[v]) continue;
+        dfs(v);
+        cnt[u] += cnt[v];
+    }
+}
+
+int main() {
+    nextInt(n, ' ', 2, (int)1e5);
+    nextInt(k, '\n', 1, 200);
+
+    for (int i = 1; i <= n; i++) {
+        char c;
+        c = ' ';
+        if (i == n) c = '\n';
+
+        nextInt(weight[i], c, (int) -(1e9), (int) (1e9));
+    }
+
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        nextInt(u, ' ', 1, n);
+        nextInt(v, '\n', 1, n);
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    memset (cnt, 0, sizeof(cnt));
+    dfs(1);
+    for (int i= 1; i <= n; i++) assert(cnt[i] > 0);
+
+    for (int i = 1; i <= n; i++)
+        sum[i] = weight[dfs_order[i]] + sum[i - 1];
+
+    for (int i = 1; i <= n; i++) f[i] = sum[i];
+    //for (int i = 1; i <= n; i++) cerr << dfs_order[i] << " " << cnt[i] << endl;
+    for (int rep = 1; rep <= k; rep ++) {
+        for (int i = n; i >= 1; i--) {
+            int pos = i + cnt[dfs_order[i]] - 1;
+            f[pos] = max(f[pos], f[i - 1]);
+        }
+
+        for (int i = 1; i <= n; i++)
+            f[i] = max(f[i], f[i - 1] + weight[dfs_order[i]]);
+    }
+
+    cout << f[n] << endl;
+    char __c;
+    assert(!getChar(__c));
+    return 0;
+}
+
+
+
+
+
+
+
 //maximum product
 void solve()
 {
@@ -397,4 +551,72 @@ void phongbeo()
         }
     }
     cout<<add(gg[0],gg[1]);
+}
+
+
+
+
+
+
+//berland
+void dfs(vector<vpii> &graph,vvi &dp,vector<vvi> &ans,int u,int p) {
+    int k=dp[0].size();
+    dp[u][1]=0;
+    for (auto e : graph[u]) {
+        int v=e.first;
+        int ind=e.second;
+        if (v!=p) {
+            dfs(graph,dp,ans,v,u);
+            for (int i=1; i<k; ++i) {
+                dp[v][i]++;
+                ans[v][i].push_back(ind);
+            }
+            for (int i=k-1; i>=1; --i) {
+                dp[u][i]++;
+                ans[u][i].push_back(ind);
+                for (int j=1; j<=i; ++j) {
+                    if (dp[u][i-j]+dp[v][j]-1<dp[u][i]) {
+                        dp[u][i]=dp[u][i-j]+dp[v][j]-1;
+                        ans[u][i].clear();
+                        for (auto x : ans[u][i-j]) {
+                            ans[u][i].push_back(x);
+                        }
+                        for (int l=0; l<ans[v][j].size()-1; ++l) {
+                            ans[u][i].push_back(ans[v][j][l]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+ 
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    int n,k;
+    cin >> n >> k;
+    vector<vpii> graph(n);
+    for (int i=1; i<n; ++i) {
+        int a,b;
+        cin >> a >> b;
+        a--;
+        b--;
+        graph[a].push_back({b,i});
+        graph[b].push_back({a,i});
+    }
+    vvi dp(n,vi(k+1,1e9));
+    vector<vvi> ans(n,vvi(k+1));
+    dfs(graph,dp,ans,0,-1);
+    vi v;
+    int s=1e9;
+    for (int i=0; i<n; ++i) {
+        if (dp[i][k]<s) {
+            s=dp[i][k];
+            v=ans[i][k];
+        }
+    }
+    cout << s << endl;
+    for (auto item : v) cout << item << ' ';
+    cout << endl;
 }
