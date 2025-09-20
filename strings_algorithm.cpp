48b11f3ae0eacf60acb08ca9616096fcf532e4ff
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-20 21:55:56 +0600

    Update strings_algorithm.cpp

--- strings_algorithm.cpp
+++ strings_algorithm.cpp
@@ -11,4 +11,29 @@ ll getHash(string &key){
 }
 
 
+//substring hash
+int pref[N];
+int inv_pref[N];
+void compute_pref_hash(string &s, int p = 31){
+    ll p_power = 1;
+    inv_pref[0] = 1;
+    pref[0] = (s[0] - 'a' + 1);
+    for(int i=1; i<s.size(); i++){
+        p_power= (binMul(p_power, p, MOD));
+        pref[i] = (pref[i-1] +( s[i]-'a'+1)*p_power)%MOD;
+        inv_pref[i] = binExp(p_power, MOD-2);
+    }
+
+}
+ll substringHash(int l, int r, int p = 31){
+    if(l==0) return pref[r];
+    ll hash_value = (pref[r]-pref[l-1]);
+    hash_value%=MOD;
+    if(hash_value<0) hash_value+=MOD;
+    hash_value = binMul(hash_value, inv_pref[l], MOD);
+    return hash_value;
+
+}
+
+
 //
