84b8df0fe4592338968722c52f5676d568d090e5
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-20 07:25:45 +0600

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -92,8 +92,84 @@ void solve() {
 
 
 
+//cf- trees
+ll dp[40][40];
 
 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, h; cin >> n >> h;
+
+    for (int i = 0; i <= n; ++i) dp[0][i] = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            for (int k = 0; k < i; ++k)
+                dp[i][j] += dp[k][j - 1] * dp[i - k - 1][j - 1];
+        }
+    }
+    cout << dp[n][n] - dp[n][h - 1] << endl;
+    return 0;
+}
+
+
+
+
+
+
+//catalan
+vi catalan(N);
+void compute_catalan()
+{
+    catalan[0] = 1;
+    catalan[1] = 1;
+    for(int i=2; i<N; i++){
+        for(int j=0; j<i; j++){
+            catalan[i] = (catalan[i] + (catalan[j]*catalan[i-j-1])%MOD)%MOD;    
+        }
+    }
+}
+
+
+//cf-count gcd
+signed main(){
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
+        int tt = 1; cin >> tt;
+        while( tt-- ){
+                int n,m; cin >> n >> m;
+                vector<int> v(n);
+                for( auto &z : v ) cin >> z;
+                bool anso = 0;
+                for( int i = 1 ; i < n ; i++ ) if( v[i-1] % v[i] ) anso = 1;
+                if( anso ){
+                        cout << 0 << '\n';
+                        continue;
+                }
+                int x = v[0] , ans = 1;
+                vector<int> fact;
+                for( int i = 2 ; ( i * i ) <= x ; i++ ){
+                        if( !( x % i ) ) fact.push_back(i);
+                        while( !( x % i ) ) x /= i;
+                }
+                if( x != 1 ) fact.push_back(x);
+                for( int i = 1 ; i < n ; i++ ){
+                        x = v[i-1] / v[i];
+                        int cnt = 0 , mx = m / v[i];
+                        vector<int> pr;
+                        for( auto z : fact ) if( !( x % z ) ) pr.push_back(z);
+                        for( int mask = 0 ; mask < ( 1 << pr.size() ) ; mask++ ){
+                                int mul = 1;
+                                for( int i = 0 ; i < pr.size() ; i++ ) if( mask & ( 1 << i ) ) mul *= pr[i];
+                                if( __builtin_popcount(mask) & 1 ) cnt -= ( mx / mul );
+                                else cnt += ( mx / mul );
+                        }
+                        ans *= cnt , ans %= mod;
+                }
+                cout << ans << '\n';
+        }
 
 
 
@@ -201,5 +277,36 @@ int main() {
  
     return 0;
 }
+
+//pankac
+{
+int a[1001],tar[1001],i,j,k,n,t,temp,temp1;
+a[1]=tar[1]=1;
+for(i=1; i<=1000; i++)
+{
+for(j=2; j<=i; j++)
+{
+temp=tar[j];
+tar[j]=tar[j-1]+temp1;
+if(tar[j]>=1000000007)
+tar[j]=tar[j]%1000000007;
+temp1=temp;
+}
+temp1=tar[1];
+a[i]=tar[1]=tar[i];
+}
+scanf(“%d”,&t);
+while(t)
+{
+scanf(“%d”,&n);
+printf(“%d\n”,a[n]);
+t–;
+}
+
+
+
+
+
+
 
 
