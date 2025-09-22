407e6240f3c355b881c5f5a4bc8678a39f14ded4
rayyankah <167612400+rayyankah@users.noreply.github.com>
2025-09-22 07:44:18 +0600

    Update TRIE.cpp

--- TRIE.cpp
+++ TRIE.cpp
@@ -132,5 +132,102 @@ solve()
 
 
 
+//vasilly's multiset
+const int SIZE = 26;
+struct TrieNode
+{
+    int count;
+    int val;
+    TrieNode *children[2];
+    TrieNode()
+    {
+        count = 0;
+        val = 1;
+        for (int i = 0; i < 2; i++)
+        {
+            children[i] = nullptr;
+        }
+    }
+};
+class Trie
+{
+private:
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        root = new TrieNode();
+        root->val = 0;
+    }
+    void insert(int n)
+    {
+        TrieNode *temp = root;
+        for(int i=30; i>=0;i--){
+            int bit = (1<<i) & n ? 1:0;
+            if(temp->children[bit]==nullptr){
+                temp->children[bit]= new TrieNode();
+
+            }
+            temp = temp->children[bit];
+            temp->count++;
+
+        }
+        temp->val = n;
+    }
+    void erase(int n)
+    {
+        TrieNode *temp = root;
+        for(int i=30; i>=0; i--){
+            int bit = ((1<<i) &n ?1:0);
+
+            temp = temp->children[bit];
+            temp->count--;
+
+        }
+        
+    }
+    int getMaxXor(int n){
+        TrieNode* temp = root;
+        for(int i=30; i>=0; i--){
+            int bit = ((1<<i) & n) ? 1 : 0;
+            int pref = bit ^ 1;
+            if(temp->children[pref] && temp->children[pref]->count > 0){
+                temp = temp->children[pref];
+            } else if(temp->children[bit] && temp->children[bit]->count > 0){
+                temp = temp->children[bit];
+            } else {
+                break;
+            }
+        }
+        return temp->val ^ n;
+    }
+
+};
+
+//================ Code starts here ================
+void
+solve()
+{
+    Trie trie;
+    int q;
+    cin >>q;
+    trie.insert(0);
+    while(q--){
+       char ch;
+       int n;
+       cin >>ch>>n;
+       if(ch=='+'){
+        trie.insert(n);
+       }
+       else if(ch=='-'){
+        trie.erase(n);
+       }
+       else{
+        cout<<trie.getMaxXor(n)<<nl;
+       }
+    }
+
+}
 
 
