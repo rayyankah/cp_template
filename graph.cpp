21de3244d99cda1b17c95f2df8942075896112e8
Rayyan Khalil <167612400+rayyankah@users.noreply.github.com>
2026-01-04 17:28:03 +0600

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -546,139 +546,6 @@ int solve_min_edges_remove_bridges() {
     return answer;
 }
 
-// ================= Vertex-biconnected components (blocks) =================
-vi tin_bcc, low_bcc;
-int timer_bcc;
-vector<pair<int,int>> edgeStack;
-vector<vector<int>> blocks; // list of blocks, each block is a list of vertices
-vector<int> isArt; // 1 if articulation point
-
-void dfs_bcc(int u, int p) {
-    tin_bcc[u] = low_bcc[u] = ++timer_bcc;
-    int children = 0;
-    for (int v : adj[u]) {
-        if (v == p) continue;
-        if (!tin_bcc[v]) {
-            // tree edge
-            edgeStack.emplace_back(u,v);
-            ++children;
-            dfs_bcc(v, u);
-            low_bcc[u] = min(low_bcc[u], low_bcc[v]);
-
-            if (low_bcc[v] >= tin_bcc[u]) {
-                // u is articulation for this block (unless u is root handled below)
-                if (p != -1) isArt[u] = 1;
-                // pop edges until (u,v) included
-                vector<int> blockVertices;
-                while (!edgeStack.empty()) {
-                    auto e = edgeStack.back(); edgeStack.pop_back();
-                    blockVertices.push_back(e.first);
-                    blockVertices.push_back(e.second);
-                    if (e.first==u && e.second==v) break;
-                }
-                // deduplicate vertices in block
-                sort(blockVertices.begin(), blockVertices.end());
-                blockVertices.erase(unique(blockVertices.begin(), blockVertices.end()), blockVertices.end());
-                blocks.push_back(blockVertices);
-            }
-        } else if (tin_bcc[v] < tin_bcc[u]) {
-            // back edge to ancestor; push once
-            edgeStack.emplace_back(u,v);
-            low_bcc[u] = min(low_bcc[u], tin_bcc[v]);
-        }
-    }
-    if (p == -1 && children > 1) isArt[u] = 1;
-}
-
-int solve_min_edges_remove_articulation() {
-    // run biconnected algorithm
-    tin_bcc.assign(n+1, 0);
-    low_bcc.assign(n+1, 0);
-    isArt.assign(n+1, 0);
-    timer_bcc = 0;
-    edgeStack.clear();
-    blocks.clear();
-
-    for (int i=1;i<=n;i++) {
-        if (!tin_bcc[i]) {
-            dfs_bcc(i, -1);
-            // after finishing a connected component, there might remain edges (a single block)
-            if (!edgeStack.empty()) {
-                vector<int> blockVertices;
-                while (!edgeStack.empty()) {
-                    auto e = edgeStack.back(); edgeStack.pop_back();
-                    blockVertices.push_back(e.first);
-                    blockVertices.push_back(e.second);
-                }
-                sort(blockVertices.begin(), blockVertices.end());
-                blockVertices.erase(unique(blockVertices.begin(), blockVertices.end()), blockVertices.end());
-                blocks.push_back(blockVertices);
-            }
-        }
-    }
-
-    if (blocks.empty()) return 0; // no edges at all (all isolated vertices) -> 0
-
-    // For each block compute how many articulation points it contains
-    int B = (int)blocks.size();
-    vi degBlock(B, 0);
-    vi connOfBlock(B, 0); // which original connected component the block belongs to
-
-    for (int i=0;i<B;i++) {
-        int connIdForThisBlock = 0;
-        for (int v: blocks[i]) {
-            if (isArt[v]) degBlock[i]++;
-            if (connIdForThisBlock==0) connIdForThisBlock = compOfNode[v];
-        }
-        connOfBlock[i] = connIdForThisBlock;
-    }
-
-    // group blocks by original connected component
-    unordered_map<int, vector<int>> blocksByConn;
-    blocksByConn.reserve(B*2);
-    for (int i=0;i<B;i++) blocksByConn[connOfBlock[i]].push_back(i);
-
-    int answer = 0;
-    for (auto &pr : blocksByConn) {
-        auto &vec = pr.second;
-        if (vec.size() == 1) continue; // single block => already biconnected inside that connected component
-        int leaves = 0;
-        for (int idx : vec) if (degBlock[idx] == 1) ++leaves;
-        if (leaves == 0) continue;
-        answer += (leaves + 1) / 2;
-    }
-    return answer;
-}
-
-// ================= Example driver (read graph, run both) =================
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    // read n, m
-    if (!(cin >> n >> m)) return 0;
-    adj.assign(n+1, {});
-    for (int i=0;i<m;i++){
-        int u,v; cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    // mark original connected components (used later)
-    mark_connected_components();
-
-    int edges_to_fix_bridges = solve_min_edges_remove_bridges();
-    int edges_to_fix_articulation = solve_min_edges_remove_articulation();
-
-    cout << "min edges to add to remove ALL bridges (make 2-edge-connected): " << edges_to_fix_bridges << "\n";
-    cout << "min edges to add to remove ALL articulation points (make biconnected): " << edges_to_fix_articulation << "\n";
-
-    return 0;
-}
-
-
-
-
 
 
 // finding eulerian path/circuit
@@ -1071,6 +938,341 @@ int main() {
  }
     return 0;
 }
+
+
+
+
+//online:
+//cf- dijkstra?
+// ================ Author: Rayyan Khalil ================
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define si set<int>
+#define mii map<int, int>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define pi pair<int, int>
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define tk_input(a) rep(i, 0, n) cin >> a[i]
+#define repb(i, a, b) for (int i = a; i >= b; i--)
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+#ifdef DEBUG
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_print(__VA_ARGS__)
+void debug_print() { cerr << endl; }
+template <typename T, typename... Args>
+void debug_print(T t, Args... args)
+{
+    cerr << t;
+    if (sizeof...(args))
+        cerr << ", ";
+    debug_print(args...);
+}
+#else
+#define debug(...)
+#endif
+
+#define myCeil(a, b) (a + b - 1) / b
+const int MOD = 1000000007;
+#define nl '\n'
+#define precise(x) fixed << setprecision(x)
+#define int ll
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+//rnd()  generates a random number
+int binMul(int a, int b, int m)
+{
+    int res = 0;
+    a %= m;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = (res + a) % m;
+        }
+        a = (a + a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int binExp(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = (1LL * res * a) % MOD;
+        }
+        a = (1LL * a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+int extended_euclidean_gcd(int a, int b, int &x, int &y)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclidean_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+template <class T>
+using vv = vector<vector<T>>;
+template <class T>
+using v = vector<T>;
+
+// ------------------ SAFE unordered_map helpers ------------------
+struct custom_hash
+{
+    static inline uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15ULL;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const noexcept
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return (size_t)splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <class K, class V>
+using umap = unordered_map<K, V, custom_hash>;
+template <class T>
+using uset = unordered_set<T, custom_hash>;
+
+template <class Map>
+inline void secure_reserve(Map &m, size_t expected_size, float lf = 0.25f)
+{
+    m.max_load_factor(lf);
+    m.reserve(expected_size);
+}
+
+template <class T>
+void read(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+template <class T>
+void read(vv<T> &a)
+{
+    for (auto &r : a)
+        for (auto &x : r)
+            cin >> x;
+}
+
+template <class A, class B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &a)
+{
+    os << "[";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "]";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class K, class V>
+ostream &operator<<(ostream &os, const map<K, V> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &kv : a)
+    {
+        os << s << kv.first << ": " << kv.second;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+
+template <class K, class V, class H, class Eq, class Alloc>
+ostream &operator<<(ostream &os, const unordered_map<K, V, H, Eq, Alloc> &a)
+{
+    os << "{";
+    string s = "";
+    for (auto &kv : a)
+    {
+        os << s << kv.first << ": " << kv.second;
+        s = ", ";
+    }
+    os << "}";
+    return os;
+}
+template <class T, size_t N>
+ostream &operator<<(ostream &os, const array<T, N> &a)
+{
+    os << "[";
+    string s = "";
+    for (auto &x : a)
+    {
+        os << s << x;
+        s = ", ";
+    }
+    os << "]";
+    return os;
+}
+
+// template<typename T>
+// using ordered_set = tree<T, null_type, //less<T>, rb_tree_tag, //tree_order_statistics_node_update>;
+// ans+=b.size()-b.order_of_key(in[i].second+1);
+
+template <class T>
+void debug_print_array(vector<T> &arr)
+{
+    for (auto &elem : arr)
+    {
+        cerr << elem << " ";
+    }
+    cerr << endl;
+}
+const int N = 1e6 + 10;
+vi factorial(N);
+vi inv(N);
+void compute_factorials()
+{
+    factorial[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        factorial[i] = (factorial[i - 1] * i) % MOD;
+    }
+    inv[N - 1] = binExp(factorial[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 0; i--)
+    {
+        inv[i] = (inv[i + 1] * (i + 1)) % MOD;
+    }
+}
+int ncr(int n, int r)
+{
+    if (r > n || r < 0)
+        return 0;
+ 
+    return (binMul(factorial[n], binMul(inv[r], inv[n - r], MOD), MOD));
+}
+//random number within a range [l,r]
+int rndRange(int l, int r) { return rnd() % (r - l + 1) + l; }
+
+
+//================ Code starts here ================
+void solve()
+{
+  int n,m;
+  cin >>n>>m;
+  vector<vector<pi>>adj(n+1);
+  rep(i,0,m){
+    int u,v,w;
+    cin >>u>>v>>w;
+    adj[u].pb({v,w});
+    adj[v].pb({u,w});
+  }
+  set<pi>pq;
+  vi par(n+1,-1);
+  par[1]=0;
+  vi dist(n+1,LLONG_MAX);
+  dist[1]=0;
+  pq.insert({0,1});
+  while(!pq.empty()){
+    pi v = *pq.begin();
+    pq.erase(pq.begin());
+    for(auto &ele: adj[v.second]){
+        int wt = ele.second;
+        if(dist[ele.first]>dist[v.second]+wt){
+            dist[ele.first] = dist[v.second]+wt;
+            par[ele.first] = v.second;
+            pq.insert({dist[ele.first],ele.first});
+        }
+    }
+  }
+  if(dist[n]==LLONG_MAX){
+    cout<<-1<<nl;
+    return;
+  }
+  vi ans;
+  ans.pb(n);
+  int now = par[n];
+  while(now!=0){
+    ans.pb(now);
+    now = par[now];
+  }
+  reverse(all(ans));
+  for(auto &ele: ans){
+    cout<<ele<<" ";
+  }
+  cout<<nl;
+
+}
+
+signed main()
+{
+    fast;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
+
 
 
 
