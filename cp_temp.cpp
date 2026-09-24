0aabf9d10ae686691959f3822d872e1b6646dc37
Rayyan Khalil <167612400+rayyankah@users.noreply.github.com>
2026-09-24 08:38:29 +0600

    polish temp

--- cp_temp.cpp
+++ cp_temp.cpp
@@ -1,283 +1,222 @@
 // ================ Author: Rayyan Khalil ================
+#define DEBUG   // <-- comment this ONE line to disable every debug(...)
 
 #include <bits/stdc++.h>
 // #include <ext/pb_ds/assoc_container.hpp>
 // #include <ext/pb_ds/tree_policy.hpp>
 // using namespace __gnu_pbds;
+// template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 using namespace std;
 
+#define ll long long
+#define int ll
+#define vi vector<int>
 #define vvi vector<vector<int>>
-#define vll vector<long long>
+#define pi pair<int, int>
+#define vpi vector<pair<int, int>>
 #define si set<int>
 #define mii map<int, int>
-#define vi vector<int>
-#define vpi vector<pair<int, int>>
-#define pi pair<int, int>
-#define ll long long
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = a; i < b; i++)
-#define tk_input(a) rep(i, 0, n) cin >> a[i]
 #define repb(i, a, b) for (int i = a; i >= b; i--)
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(nullptr);                 \
-    cout.tie(nullptr)
+#define myCeil(a, b) (((a) + (b) - 1) / (b))
+#define precise(x) fixed << setprecision(x)
+#define nl '\n'
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr)
 
+const int MOD = 998244353; // or 1000000007
+const int N = 1e6 + 10;
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int rndRange(int l, int r) { return rnd() % (r - l + 1) + l; } // random in [l,r]
+
+// ================ Debug ================
+// debug(x, v, st, mp, ...)  -> prints anything: ints, strings, pairs, vector, set, map, nested...
+// debugArr(a, n)            -> prints first n elements of a C array
 #ifdef DEBUG
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_print(__VA_ARGS__)
-void debug_print() { cerr << endl; }
-template <typename T, typename... Args>
-void debug_print(T t, Args... args)
+template <class T, class = void> struct is_iterable : false_type {};
+template <class T> struct is_iterable<T, void_t<decltype(begin(declval<T &>())), decltype(end(declval<T &>()))>> : true_type {};
+
+template <class A, class B> void _print(const pair<A, B> &p);
+template <class T> void _print(const T &x)
 {
-    cerr << t;
-    if (sizeof...(args))
-        cerr << ", ";
-    debug_print(args...);
+    if constexpr (is_iterable<T>::value && !is_same_v<T, string>)
+    {
+        cerr << "{";
+        bool first = true;
+        for (auto &&e : x)
+        {
+            if (!first) cerr << ", ";
+            first = false;
+            _print(e);
+        }
+        cerr << "}";
+    }
+    else cerr << x;
 }
+template <class A, class B> void _print(const pair<A, B> &p)
+{
+    cerr << "(";
+    _print(p.first);
+    cerr << ", ";
+    _print(p.second);
+    cerr << ")";
+}
+void _dbg() { cerr << nl; }
+template <class H, class... T> void _dbg(const H &h, const T &...t)
+{
+    _print(h);
+    if (sizeof...(t)) cerr << " | ";
+    _dbg(t...);
+}
+#define debug(...) cerr << "L" << __LINE__ << " [" << #__VA_ARGS__ << "] = ", _dbg(__VA_ARGS__)
+#define debugArr(a, n) cerr << "L" << __LINE__ << " [" << #a << "] = ", _dbg(vector<decay_t<decltype(a[0])>>(a, a + (n)))
 #else
 #define debug(...)
+#define debugArr(a, n)
 #endif
 
-#define myCeil(a, b) (a + b - 1) / b
-const int MOD = 1000000007;
-#define nl '\n'
-#define precise(x) fixed << setprecision(x)
-#define int ll
-mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
-//rnd()  generates a random number
+template <class T> void read(vector<T> &a) { for (auto &x : a) cin >> x; }
+
+// ================ Math ================
+int binExp(int a, int b, int m = MOD)
+{
+    int res = 1;
+    a %= m;
+    while (b > 0)
+    {
+        if (b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+// a*b % m in O(log b), safe for a, b, m up to 1e18
 int binMul(int a, int b, int m)
 {
     int res = 0;
     a %= m;
     while (b > 0)
     {
-        if (b & 1)
-        {
-            res = (res + a) % m;
-        }
+        if (b & 1) res = (res + a) % m;
         a = (a + a) % m;
         b >>= 1;
     }
     return res;
 }
-
-int binExp(int a, int b)
+int extgcd(int a, int b, int &x, int &y)
 {
-    int res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-        {
-            res = (1LL * res * a) % MOD;
-        }
-        a = (1LL * a * a) % MOD;
-        b >>= 1;
-    }
-    return res;
-}
-int extended_euclidean_gcd(int a, int b, int &x, int &y)
-{
-    if (b == 0)
-    {
-        x = 1;
-        y = 0;
-        return a;
-    }
+    if (b == 0) { x = 1, y = 0; return a; }
     int x1, y1;
-    int d = extended_euclidean_gcd(b, a % b, x1, y1);
+    int d = extgcd(b, a % b, x1, y1);
     x = y1;
     y = x1 - y1 * (a / b);
     return d;
 }
 
-template <class T>
-using vv = vector<vector<T>>;
-template <class T>
-using v = vector<T>;
+int fact[N], invFact[N];
+void compute_factorials() // call once in main if you need ncr
+{
+    fact[0] = 1;
+    rep(i, 1, N) fact[i] = fact[i - 1] * i % MOD;
+    invFact[N - 1] = binExp(fact[N - 1], MOD - 2);
+    repb(i, N - 2, 0) invFact[i] = invFact[i + 1] * (i + 1) % MOD;
+}
+int ncr(int n, int r)
+{
+    if (r < 0 || r > n) return 0;
+    return fact[n] * invFact[r] % MOD * invFact[n - r] % MOD;
+}
 
-// ------------------ SAFE unordered_map helpers ------------------
+// ================ Mint ================
+struct Mint
+{
+    int value;
+    Mint(ll v = 0) { value = v % MOD; if (value < 0) value += MOD; }
+    Mint(ll a, ll b) : value(0) { *this += a; *this /= b; } // a/b mod MOD
+
+    Mint &operator+=(const Mint &b) { value += b.value; if (value >= MOD) value -= MOD; return *this; }
+    Mint &operator-=(const Mint &b) { value -= b.value; if (value < 0) value += MOD; return *this; }
+    Mint &operator*=(const Mint &b) { value = value * b.value % MOD; return *this; }
+    Mint &operator/=(const Mint &b) { return *this *= inverse(b); }
+
+    friend Mint mexp(Mint a, ll e) { Mint res = 1; while (e) { if (e & 1) res *= a; a *= a; e >>= 1; } return res; }
+    friend Mint inverse(Mint a) { return mexp(a, MOD - 2); }
+
+    friend Mint operator+(Mint a, const Mint &b) { return a += b; }
+    friend Mint operator-(Mint a, const Mint &b) { return a -= b; }
+    friend Mint operator*(Mint a, const Mint &b) { return a *= b; }
+    friend Mint operator/(Mint a, const Mint &b) { return a /= b; }
+    friend Mint operator-(const Mint &a) { return Mint(0) - a; }
+    friend bool operator==(const Mint &a, const Mint &b) { return a.value == b.value; }
+    friend bool operator!=(const Mint &a, const Mint &b) { return a.value != b.value; }
+    friend bool operator<(const Mint &a, const Mint &b) { return a.value < b.value; }
+    friend ostream &operator<<(ostream &os, const Mint &a) { return os << a.value; }
+};
+
+// ================ Sieve ================
+int spf[N];
+bool isPrime[N];
+vector<vpi> distinct_primes(N); // distinct_primes[x] = {prime, count} of x
+vpi factorize(int x);
+void sieve() // call once in main
+{
+    rep(i, 2, N)
+    {
+        if (spf[i] == 0)
+        {
+            spf[i] = i;
+            isPrime[i] = 1;
+            for (int j = i * i; j < N; j += i)
+                if (spf[j] == 0) spf[j] = i;
+        }
+    }
+    rep(i, 2, N) distinct_primes[i] = factorize(i);
+}
+vpi factorize(int x) // {prime, count}, O(log x), needs sieve()
+{
+    vpi res;
+    while (x > 1)
+    {
+        int p = spf[x], cnt = 0;
+        while (x % p == 0) x /= p, cnt++;
+        res.pb({p, cnt});
+    }
+    return res;
+}
+
+// ================ Anti-hack unordered_map ================
 struct custom_hash
 {
-    static inline uint64_t splitmix64(uint64_t x)
+    static uint64_t splitmix64(uint64_t x)
     {
         x += 0x9e3779b97f4a7c15ULL;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
         x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
         return x ^ (x >> 31);
     }
-    size_t operator()(uint64_t x) const noexcept
+    size_t operator()(uint64_t x) const
     {
-        static const uint64_t FIXED_RANDOM =
-            chrono::steady_clock::now().time_since_epoch().count();
-        return (size_t)splitmix64(x + FIXED_RANDOM);
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
     }
 };
-
-template <class K, class V>
-using umap = unordered_map<K, V, custom_hash>;
-template <class T>
-using uset = unordered_set<T, custom_hash>;
-
-template <class Map>
-inline void secure_reserve(Map &m, size_t expected_size, float lf = 0.25f)
-{
-    m.max_load_factor(lf);
-    m.reserve(expected_size);
-}
-
-template <class T>
-void read(vector<T> &a)
-{
-    for (auto &x : a)
-        cin >> x;
-}
-template <class T>
-void read(vv<T> &a)
-{
-    for (auto &r : a)
-        for (auto &x : r)
-            cin >> x;
-}
-
-template <class A, class B>
-ostream &operator<<(ostream &os, const pair<A, B> &p)
-{
-    os << "(" << p.first << ", " << p.second << ")";
-    return os;
-}
-template <class T>
-ostream &operator<<(ostream &os, const vector<T> &a)
-{
-    os << "[";
-    string s = "";
-    for (auto &x : a)
-    {
-        os << s << x;
-        s = ", ";
-    }
-    os << "]";
-    return os;
-}
-template <class T>
-ostream &operator<<(ostream &os, const set<T> &a)
-{
-    os << "{";
-    string s = "";
-    for (auto &x : a)
-    {
-        os << s << x;
-        s = ", ";
-    }
-    os << "}";
-    return os;
-}
-template <class T>
-ostream &operator<<(ostream &os, const multiset<T> &a)
-{
-    os << "{";
-    string s = "";
-    for (auto &x : a)
-    {
-        os << s << x;
-        s = ", ";
-    }
-    os << "}";
-    return os;
-}
-template <class K, class V>
-ostream &operator<<(ostream &os, const map<K, V> &a)
-{
-    os << "{";
-    string s = "";
-    for (auto &kv : a)
-    {
-        os << s << kv.first << ": " << kv.second;
-        s = ", ";
-    }
-    os << "}";
-    return os;
-}
-
-template <class K, class V, class H, class Eq, class Alloc>
-ostream &operator<<(ostream &os, const unordered_map<K, V, H, Eq, Alloc> &a)
-{
-    os << "{";
-    string s = "";
-    for (auto &kv : a)
-    {
-        os << s << kv.first << ": " << kv.second;
-        s = ", ";
-    }
-    os << "}";
-    return os;
-}
-template <class T, size_t N>
-ostream &operator<<(ostream &os, const array<T, N> &a)
-{
-    os << "[";
-    string s = "";
-    for (auto &x : a)
-    {
-        os << s << x;
-        s = ", ";
-    }
-    os << "]";
-    return os;
-}
-
-// template<typename T>
-// using ordered_set = tree<T, null_type, //less<T>, rb_tree_tag, //tree_order_statistics_node_update>;
-// ans+=b.size()-b.order_of_key(in[i].second+1);
-
-template <class T>
-void debug_print_array(vector<T> &arr)
-{
-    for (auto &elem : arr)
-    {
-        cerr << elem << " ";
-    }
-    cerr << endl;
-}
-const int N = 1e6 + 10;
-vi factorial(N);
-vi inv(N);
-void compute_factorials()
-{
-    factorial[0] = 1;
-    for (int i = 1; i < N; i++)
-    {
-        factorial[i] = (factorial[i - 1] * i) % MOD;
-    }
-    inv[N - 1] = binExp(factorial[N - 1], MOD - 2);
-    for (int i = N - 2; i >= 0; i--)
-    {
-        inv[i] = (inv[i + 1] * (i + 1)) % MOD;
-    }
-}
-int ncr(int n, int r)
-{
-    if (r > n || r < 0)
-        return 0;
- 
-    return (binMul(factorial[n], binMul(inv[r], inv[n - r], MOD), MOD));
-}
-//random number within a range [l,r]
-int rndRange(int l, int r) { return rnd() % (r - l + 1) + l; }
-
+template <class K, class V> using umap = unordered_map<K, V, custom_hash>;
+template <class T> using uset = unordered_set<T, custom_hash>;
 
 //================ Code starts here ================
 void solve()
 {
-  
+
 }
 
 signed main()
 {
     fast;
-
+    // sieve();
+    // compute_factorials();
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
     {
         solve();
